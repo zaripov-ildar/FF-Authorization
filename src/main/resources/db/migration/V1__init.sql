46fdf915c3544dd3b2729784b9090b78aab90cf4
ildar <zariil@yandex.ru>
2023-07-07 12:11:44 +0500

    tables is created

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS _role
+(
+    id         serial primary key,
+    title      varchar(50),
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp
+
+);
+CREATE TABLE IF NOT EXISTS auth_user
+(
+    id         serial primary key,
+    email      varchar(50),
+    password   varchar(100),
+    created_at timestamp DEFAULT current_timestamp,
+    updated_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS user_role
+(
+    user_id bigint REFERENCES auth_user (id),
+    role_id bigint REFERENCES _role (id)
+);
